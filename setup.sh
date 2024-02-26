5ade20b187e1dcfe219d6fb59fde5522c304d92f
Justin Choi <justin.choi@fastly.com>
2024-02-25 23:33:34 -0500

    add in mac alternate

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ## setup.sh to setup any new machine that you want to import dotfiles into
 ## this is for linux machines, will have mac alternates in comments
-# brew install fzf ripgrep stow
+# brew install fzf ripgrep stow neovim
 
 sudo apt-get update && sudo apt-get install stow fzf ripgrep neovim
 git clone https://github.com/orangejuicetin/.dotfiles.git 
